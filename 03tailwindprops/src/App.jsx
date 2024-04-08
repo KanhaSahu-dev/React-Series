31c431bfeb8322d491efda518a24b738274c6afe
Kanha <kanhasahu273273@gmail.com>
2024-04-08 08:23:58 +0530

    Passing objects to child component

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,17 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
 import './App.css';
 import Card from './components/Cards';
 
 function App() {
+  const boi = {
+    name: "Kanha",
+    hello: "yo"
+  };
 
   return (
     <>
-      <h1 className='text-3xl bg-green-50 p-3 rounded-md'>Vite with TailWind</h1>
+      <Card boi={boi} />
+      <Card boi={boi} />
+      <Card />
       <Card />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- 03tailwindprops/src/components/Cards.jsx
+++ 03tailwindprops/src/components/Cards.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React from 'react';
 
-function Card() {
+function Card({ boi }) {
+  // Check if boi is defined before accessing its properties
+  if (!boi) {
+    return <div>No data available</div>;
+  }
+
   return (
     <div>
-        <img src="https://images.pexels.com/photos/4788287/pexels-photo-4788287.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
-        <h1 className='text-2xl bg-green-500 p-3 rounded'>A card for Photos</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, temporibus!</p>
+      <h1 className='text-3xl bg-green-50 p-3 rounded-md'>
+        Vite with TailWind-{boi.name}-{boi.hello}
+      </h1>
+      <img
+        src="https://images.pexels.com/photos/4788287/pexels-photo-4788287.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+        height="50"
+        width="100"
+        alt=""
+      />
+      <h1 className='text-2xl bg-green-500 p-3 rounded'>A card for Photos</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, temporibus!</p>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
